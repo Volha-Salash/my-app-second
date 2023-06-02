30995cf3a5730a4758080b6937a830f8ec9fe48d
Kursonchik <olgakurson@mail.ru>
2023-06-02 05:17:42 -0700

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      board: Array(9)
+      .fill(null), xIsNext: true,
+  };
 }
 
-export default App;
+handleClick(index){
+  const squares = [this.state.board];
+  //если кто то выиграл игнорим клик
+  if (calculateWinner(squares) || squares[index]) {
+    return
+  }
+  //записываем текущий символ в выбранную клетку
+  squares[index] = this.state.xIsNext ? 'X' : '0';
+  this.setState({
+    board: squares,
+    xIsNext : !this.state.xIsNext,
+  });
+}
+
+
+  renderSquare(index){
+    return (
+      <button className='square' onClick={() => this.handleClick(index)}>
+        {this.state.board[index]}
+      </button>
+    );
+  }
+
+  render(){
+    const winner = calculateWinner(this.state.board);
+    const status = winner ? `Winner:${winner}` : `Next player:${this.state.xIsNext ? 'X' : '0'}`;
+
+    return (
+      <div className='game'>
+        <div className='game board'>
+          <div className='board-row'>
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className='board-row'>
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className='board-row'>
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+        </div>
+        <div className='game-info'>
+          <div>{status}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  export default App;
