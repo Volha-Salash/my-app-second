c618b9b94f3eba130c2911cc56aaf90647b99049
Kursonchik <olgakurson@mail.ru>
2023-06-02 06:51:56 -0700

    3

---
+++ src/Board.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+function Square(props) {
+    const className = 'square' + (props.highlight ? ' highlight' : '');
+    return (
+      <button
+        className={className}
+        onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
+  }
+
+class Board extends React.Component {
+    renderSquare(i) {
+      const winLine = this.props.winLine;
+      return (
+        <Square
+          key={i}
+          value={this.props.squares[i]}
+          onClick={() => this.props.onClick(i)}
+          highlight={winLine && winLine.includes(i)}
+        />
+      );
+    }
+  
+    render() {
+      // Use two loops to make the squares
+      const boardSize = 3;
+      let squares = [];
+      for (let i = 0; i < boardSize; ++i) {
+        let row = [];
+        for (let j = 0; j < boardSize; ++j) {
+          row.push(this.renderSquare(i * boardSize + j));
+        }
+        squares.push(<div key={i} className="board-row">{row}</div>);
+      }
+  
+      return (
+        <div>{squares}</div>
+      );
+    }
+    
+  }
+  
+export default Board;  
